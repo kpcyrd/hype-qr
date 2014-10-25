243fb14d107cdbfc97819645b0b51033b900044b
kpcyrd <git@rxv.cc>
2014-10-25 16:46:12 +0200

    Update qr encoder

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 (function() {
-	var elem = document.getElementById('connectstr');
+	var preview = document.getElementById('preview');
+	var host = document.getElementById('host');
+	var pk = document.getElementById('pk');
+	var pw = document.getElementById('pw');
 
     function update() {
+        var txt = 'cjdns://' + host.value + '?pk=' + escape(pk.value) + '&pw=' + escape(pw.value);
+        preview.textContent = txt;
+
 		var canvas = document.getElementById('qr');
 		qrcode = new qrcodelib.qrcodedraw()
-		qrcode.draw(canvas, elem.value, function() {});
+		qrcode.draw(canvas, txt, function() {});
     }
 
-	elem.addEventListener('keyup', update);
+	host.addEventListener('keyup', update);
+	pk.addEventListener('keyup', update);
+	pw.addEventListener('keyup', update);
+
+    update();
 })();
